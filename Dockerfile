498dc7289d329e169a7c4677b427e8cef51e3923
Andrew Hayworth <ahayworth@gmail.com>
2019-11-13 17:04:20 -0600

    build in chroot

--- Dockerfile
+++ Dockerfile
@@ -16,8 +16,8 @@ WORKDIR /home/builder
 RUN gpg --recv-keys DBE7D3DD8C81D58D0A13D0E76BC26A17B9B7018A
 
 RUN cd /tmp \
-      && git clone --depth 1 https://aur.archlinux.org/aurutils.git \
-      && cd aurutils \
+      && git clone --depth 1 https://aur.archlinux.org/aurutils-git.git \
+      && cd aurutils-git \
       && makepkg -si --noconfirm
 
 RUN cd /tmp \
@@ -29,3 +29,9 @@ RUN cd /tmp \
       && git clone --depth 1 https://aur.archlinux.org/google-cloud-sdk.git \
       && cd google-cloud-sdk \
       && makepkg -si --noconfirm
+
+RUN sudo /bin/bash -c "echo -e '[ahayworth]\nSigLevel = Optional TrustAll\nServer = file:///repo' >> /etc/pacman.conf"
+RUN repo-add /repo/ahayworth.db.tar
+RUN sudo pacman -Syu
+RUN sudo ln -sf /usr/sbin/archbuild /usr/local/bin/aur-x86_64-build
+RUN sudo cp /etc/pacman.conf /usr/share/devtools/pacman-aur.conf
--- script/build-packages.sh
+++ script/build-packages.sh
@@ -1,24 +1,45 @@
 #!/bin/bash
 
+REPO_NAME='ahayworth'
+REPO_LOC='/repo'
+
 function buildpkg() {
-  aur sync -u --noview --noconfirm -d ahayworth --root /repo "$1"
+  aur sync \
+    --upgrades \
+    --chroot \
+    --bind-rw $REPO_LOC \
+    --temp \
+    --remove \
+    --noview \
+    --noconfirm \
+    --database $REPO_NAME \
+    --root $REPO_LOC \
+    "$1"
 }
 
 function listrepo() {
-  aur repo -d ahayworth -r /repo -l
+  aur repo -d $REPO_NAME -r $REPO_LOC -l | egrep -v '^\-'
 }
 
 function fix_symlinks() {
   for e in db files; do
-    rm -f /repo/ahayworth.$e /repo/ahayworth.$e.tar.old
-    ln -sf /repo/ahayworth.$e.tar /repo/ahayworth.$e
+    rm -f $REPO_LOC/$REPO_NAME.$e $REPO_LOC/$REPO_NAME.$e.tar.old
+    ln -sf $REPO_LOC/$REPO_NAME.$e.tar $REPO_LOC/$REPO_NAME.$e
   done
 }
 
-echo $GCLOUD_CREDENTIALS | base64 -d > /tmp/gcloud.json
-gcloud auth activate-service-account --key-file /tmp/gcloud.json
-gsutil -m rsync -d -r gs://archlinux-packages /repo
+function gcloud_login() {
+  echo $GCLOUD_CREDENTIALS | base64 -d > /tmp/gcloud.json
+  gcloud auth activate-service-account --key-file /tmp/gcloud.json
+}
 
+function gcloud_sync() {
+  gsutil -m rsync -d -r "$1" "$2"
+}
+
+gcloud_login
+
+gcloud_sync gs://archlinux-packages $REPO_LOC
 fix_symlinks
 
 listrepo | aur vercmp -q > /tmp/needs_update
@@ -37,13 +58,13 @@ done < /home/builder/repo/packages
 while read pkg ver; do
   if ! grep -q "$pkg" /home/builder/repo/packages; then
     echo "Removing $pkg..."
-    repo-remove /repo/ahayworth.db.tar $pkg
+    repo-remove $REPO_LOC/$REPO_NAME.db.tar $pkg
+    rm -f $REPO_LOC/$pkg*.pkg.tar.*
   fi
 done < <(listrepo)
 
 fix_symlinks
-
-gsutil -m rsync -d -r /repo gs://archlinux-packages
+gcloud_sync $REPO_LOC gs://archlinux-packages
 
 echo "Done! Current packages:"
 listrepo
